c2c5e5c6fe5b1815228054d047423fea8aedd5ec
Brayton Whitlock <brayton94@outlook.com>
2023-10-03 19:13:33 -0700

    lights-out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Board from "./Board";
+import "./App.css";
+
+/** Simple app that just shows the LightsOut game. */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to lights-out</h1>
+      <p>The goal of this game is to turn off all the lights on the grid! Here is the catch, when you change the state of a light, it will also change the state of it's adjacent lights.😱 Good luck!!</p>
+      <Board />
     </div>
   );
 }
